d78c4108c0e10c8905fcd133bded1dde0507af62
Hasanadnan <71681524+Hasanadnan@users.noreply.github.com>
2022-02-22 22:54:49 +0600

    Solution added

--- Problem2Solution.sql
+++ Problem2Solution.sql
@@ -1,6 +1,6 @@
 
 
-Solution 1:
+--Solution 1:
 with a as
 (select *, row_number() over (order by id) an from Students_Table),
 b as
@@ -11,6 +11,8 @@ select a.Name, b.Category, (CASE WHEN a.Score > 45 THEN 'Passed' WHEN a.Score < 
 from a
 full outer join b on a.an = b.bn ORDER BY b.Category DESC
 
-Solution 2:
-SELECT Name, (CASE WHEN Score < 31 THEN 1 WHEN Score < 46 THEN 2 WHEN Score < 66 THEN 3 WHEN Score < 86 THEN 4 WHEN Score < 101 THEN 5 END) AS Category, (CASE WHEN Score > 45 THEN 'Passed' WHEN Score < 45 THEN 'Failed'
- END) AS Result FROM Students_Table ORDER BY Category DESC
+--Solution 2:
+SELECT Name, 
+       (CASE WHEN Score < 31 THEN 1 WHEN Score < 46 THEN 2 WHEN Score < 66 THEN 3 WHEN Score < 86 THEN 4 WHEN Score < 101 THEN 5 END) AS Category, 
+       (CASE WHEN Score > 45 THEN 'Passed' WHEN Score < 45 THEN 'Failed'END) AS Result
+ FROM Students_Table ORDER BY Category DESC
